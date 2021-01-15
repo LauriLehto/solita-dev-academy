ea6d3cfa6e6186234bc90739a3920af96f40c44b
laurimil <laurimilehto@gmail.com>
2021-01-15 16:35:58 +0200

    added name counts and filtered input with results

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { Container, Card, CardContent, Typography, Input } from '@material-ui/core'
+import { Container, Grid, Card, CardContent, Typography, Input, CardActions } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import MaterialTable from "material-table";
 import { tableIcons } from '../icons/Icons'
@@ -26,28 +26,47 @@ const useStyles = makeStyles({
 const Datatable = () => {
 
   const [ param, setParam ] = useState('')
+  const [ filtered, setFiltered ]= useState([])
   const classes = useStyles();
 
   const handleInput = (e) => {
     console.log(e.target.value)
+    const filtered = names.filter(n => n.name.toLowerCase().includes(e.target.value))
+    console.log(filtered)
+    setFiltered(filtered)
+    setParam(e.target.value)
   }
   return (
     <Container>
-      <Card className={classes.root}>
-        <CardContent>
-          <Typography variant="h5" component="h2">
-            Nimiä {names.length}
+      <Grid container spacing={3}>
+        <Grid item  xs={6}>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Nimiä yhteensä {names.length}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item  xs={6}>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Nimiä seulottuna {filtered.length}
+              </Typography>
+
+              
+            </CardContent>
+            <CardActions>
+
+            </CardActions>
+          </Card>
+          <Input placeholder="Etsi nimellä..." value={param} onChange={handleInput} />
+          <Typography>
+            {filtered.length!==names.length && filtered.map(f => f.name+' ')}
           </Typography>
-        </CardContent>
-      </Card>
-      <Card className={classes.root}>
-        <CardContent>
-          <Input placeholder="Etsi nimellä..." onChange={handleInput} />
-          <Typography variant="h5" component="h2">
-            Nimiä {names.length}
-          </Typography>
-        </CardContent>
-      </Card>
+        </Grid>
+      </Grid>
       <MaterialTable 
         icons={tableIcons}
         columns={[
